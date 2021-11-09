178ec716b48d4f918c2a5656cd2e0c252230baac
Atoyr <lessironglance@gmail.com>
2021-11-09 14:14:09 +0900

    update id is string to uniquidentified

--- src/backend/database/account.go
+++ src/backend/database/account.go
@@ -3,63 +3,63 @@ package database
 import (
 	"database/sql"
 	"fmt"
+
+	mssql "github.com/denisenkom/go-mssqldb"
 )
 
 type Account struct {
-	ID         string `json:"id"`
-	ProfileID  string `json:"profile_id"`
-	Signature  []byte `json:"signature"`
-	Private    []byte `json:"private"`
-	Public     []byte `json:"public"`
-	ValidFlg   bool   `json:"valid_flg"`
-	UrgeSignin bool   `json:"urge_signin"`
+	ID        string `json:"id"`
+	ProfileID string `json:"profile_id"`
+	Signature []byte `json:"signature"`
+	Private   []byte `json:"private"`
+	Public    []byte `json:"public"`
+	ValidFlg  bool   `json:"valid_flg"`
 	History
 }
 
 const getAccountQuery string = `
- select
- 	ID
- 	,ProfileID
- 	,Signature
- 	,Private
- 	,Public
- 	,ValidFlg
-	,UrgeSignin
- 	,InsertDatetime
- 	,ModifiedDatetime
- 	,InsertAccountID
- 	,InsertSystemID
- 	,ModifiedAccountID
- 	,ModifiedSystemID
- from
- 	dbo.Accounts
- `
+select
+	 [ID]
+	,[ProfileID]
+	,[Signature]
+	,[Private]
+	,[Public]
+	,[ValidFlg]
+	,[InsertDatetime]
+	,[ModifiedDatetime]
+	,[InsertAccountID]
+	,[InsertSystemID]
+	,[ModifiedAccountID]
+	,[ModifiedSystemID]
+from
+	[dbo].[Accounts]
+`
 
 const insertAccountQuery string = `
-insert into dbo.Accounts (
- 	 ProfileID
- 	,Signature
- 	,Private
- 	,Public
- 	,ValidFlg
- 	,InsertAccountID
- 	,InsertSystemID
- 	,ModifiedAccountID
- 	,ModifiedSystemID
+insert into [dbo].[Accounts] (
+	 [ProfileID]
+	,[Signature]
+	,[Private]
+	,[Public]
+	,[ValidFlg]
+	,[InsertAccountID]
+	,[InsertSystemID]
+	,[ModifiedAccountID]
+	,[ModifiedSystemID]
 	)
-	output inserted.ID
-	value (
-		,$1
-		,$2
-		,$3
-		,$4
-		,$5
-		,$6
-		,$7
-		,$8
-		,$9
-	)
- `
+output [inserted].[ID]
+value (
+	,$1
+	,$2
+	,$3
+	,$4
+	,$5
+	,$6
+	,$7
+	,$8
+	,$9
+)
+`
 
 func GetAuthenictions(tx *sql.Tx, wps WherePhrases) ([]Account, error) {
 	accounts := make([]Account, 0)
@@ -73,23 +73,34 @@ func GetAuthenictions(tx *sql.Tx, wps WherePhrases) ([]Account, error) {
 	defer rows.Close()
 	for rows.Next() {
 		account := Account{}
+		id := mssql.UniqueIdentifier{}
+		profileId := mssql.UniqueIdentifier{}
+		insertAccountId := mssql.UniqueIdentifier{}
+		insertSystemId := mssql.UniqueIdentifier{}
+		modifiedAccountId := mssql.UniqueIdentifier{}
+		modifiedSystemId := mssql.UniqueIdentifier{}
 		if err := rows.Scan(
-			account.ID,
-			account.ProfileID,
+			id,
+			profileId,
 			account.Signature,
 			account.Private,
 			account.Public,
 			account.ValidFlg,
-			account.UrgeSignin,
 			account.InsertDatetime,
 			account.ModifiedDatetime,
-			account.InsertAccountID,
-			account.InsertSystemID,
-			account.ModifiedAccountID,
-			account.ModifiedSystemID,
+			insertAccountId,
+			insertSystemId,
+			modifiedAccountId,
+			modifiedSystemId,
 		); err != nil {
 			return nil, err
 		}
+		account.ID = id.String()
+		account.ProfileID = profileId.String()
+		account.InsertAccountID = insertAccountId.String()
+		account.InsertSystemID = insertSystemId.String()
+		account.ModifiedAccountID = modifiedAccountId.String()
+		account.ModifiedSystemID = modifiedSystemId.String()
 		accounts = append(accounts, account)
 	}
 	return accounts, nil
@@ -151,22 +162,32 @@ func (account *Account) InsertOrUpdate(tx *sql.Tx) error {
 
 func (account *Account) insert(tx *sql.Tx) error {
 	query := insertAccountQuery
-	id := ""
+	id := mssql.UniqueIdentifier{}
+	profileId := mssql.UniqueIdentifier{}
+	insertAccountId := mssql.UniqueIdentifier{}
+	insertSystemId := mssql.UniqueIdentifier{}
+	modifiedAccountId := mssql.UniqueIdentifier{}
+	modifiedSystemId := mssql.UniqueIdentifier{}
+	profileId.Scan(account.ProfileID)
+	insertAccountId.Scan(account.InsertAccountID)
+	insertSystemId.Scan(account.InsertSystemID)
+	modifiedAccountId.Scan(account.ModifiedAccountID)
+	modifiedSystemId.Scan(account.ModifiedSystemID)
 	err := tx.QueryRow(
 		query,
-		account.ProfileID,
+		profileId,
 		account.Signature,
 		account.Private,
 		account.Public,
 		account.ValidFlg,
-		account.InsertAccountID,
-		account.InsertSystemID,
-		account.ModifiedAccountID,
-		account.ModifiedSystemID).Scan(&id)
+		insertAccountId,
+		insertSystemId,
+		modifiedAccountId,
+		modifiedSystemId).Scan(&id)
 	if err != nil {
 		return err
 	}
-	account.ID = id
+	account.ID = id.String()
 	return nil
 }
 
--- src/backend/database/profile.go
+++ src/backend/database/profile.go
@@ -1,6 +1,11 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	mssql "github.com/denisenkom/go-mssqldb"
+)
 
 type Profile struct {
 	ID         string `json:"id"`
@@ -12,6 +17,51 @@ type Profile struct {
 	ValidFlg   bool   `json:"valid_flg"`
 	History
 }
+
+const getProfileQuery string = `
+select
+	 [ID]
+	,[Email]
+	,[FirstName]
+	,[MiddleName]
+	,[LastName]
+	,[Nickname]
+	,[ValidFlg]
+	,[InsertDatetime]
+	,[ModifiedDatetime]
+	,[InsertAccountID]
+	,[InsertSystemID]
+	,[ModifiedAccountID]
+	,[ModifiedSystemID]
+from
+	[dbo].[Profile]
+`
+
+const insertProfileQuery string = `
+insert into [dbo].[Profile] (
+	 [Email]
+	,[FirstName]
+	,[MiddleName]
+	,[LastName]
+	,[Nickname]
+	,[InsertAccountID]
+	,[InsertSystemID]
+	,[ModifiedAccountID]
+	,[ModifiedSystemID]
+	)
+output [inserted].[ID]
+value (
+	,$1
+	,$2
+	,$3
+	,$4
+	,$5
+	,$6
+	,$7
+	,$8
+	,$9
+)
+`
 
 func GetProfiles(tx *sql.Tx, wps WherePhrases) ([]Profile, error) {
 	profiles := make([]Profile, 0)
@@ -25,8 +75,13 @@ func GetProfiles(tx *sql.Tx, wps WherePhrases) ([]Profile, error) {
 	defer rows.Close()
 	for rows.Next() {
 		profile := Profile{}
+		id := mssql.UniqueIdentifier{}
+		insertAccountId := mssql.UniqueIdentifier{}
+		insertSystemId := mssql.UniqueIdentifier{}
+		modifiedAccountId := mssql.UniqueIdentifier{}
+		modifiedSystemId := mssql.UniqueIdentifier{}
 		if err := rows.Scan(
-			profile.ID,
+			id,
 			profile.Email,
 			profile.FirstName,
 			profile.MiddleName,
@@ -35,18 +90,66 @@ func GetProfiles(tx *sql.Tx, wps WherePhrases) ([]Profile, error) {
 			profile.ValidFlg,
 			profile.InsertDatetime,
 			profile.ModifiedDatetime,
-			profile.InsertAccountID,
-			profile.InsertSystemID,
-			profile.ModifiedAccountID,
-			profile.ModifiedSystemID,
+			insertAccountId,
+			insertSystemId,
+			modifiedAccountId,
+			modifiedSystemId,
 		); err != nil {
 			return nil, err
 		}
+		profile.ID = id.String()
+		profile.InsertAccountID = insertAccountId.String()
+		profile.InsertSystemID = insertSystemId.String()
+		profile.ModifiedAccountID = modifiedAccountId.String()
+		profile.ModifiedSystemID = modifiedSystemId.String()
 		profiles = append(profiles, profile)
 	}
 	return profiles, nil
 }
 
 func (profile *Profile) Insert(tx *sql.Tx) error {
+	wps := WherePhrases{}
+	wps.Append(Equal, "ID", profile.ID)
+	profiles, err := GetProfiles(tx, wps)
+	if err != nil {
+		return err
+	}
+	if len(profiles) > 0 {
+		return fmt.Errorf("Account Exists")
+	} else {
+		err = profile.insert(tx)
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (profile *Profile) insert(tx *sql.Tx) error {
+	query := insertProfileQuery
+	id := mssql.UniqueIdentifier{}
+	insertAccountId := mssql.UniqueIdentifier{}
+	insertSystemId := mssql.UniqueIdentifier{}
+	modifiedAccountId := mssql.UniqueIdentifier{}
+	modifiedSystemId := mssql.UniqueIdentifier{}
+	insertAccountId.Scan(profile.InsertAccountID)
+	insertSystemId.Scan(profile.InsertSystemID)
+	modifiedAccountId.Scan(profile.ModifiedAccountID)
+	modifiedSystemId.Scan(profile.ModifiedSystemID)
+	err := tx.QueryRow(
+		query,
+		profile.Email,
+		profile.FirstName,
+		profile.MiddleName,
+		profile.LastName,
+		profile.Nickname,
+		insertAccountId,
+		insertSystemId,
+		modifiedAccountId,
+		modifiedSystemId).Scan(&id)
+	if err != nil {
+		return err
+	}
+	profile.ID = id.String()
 	return nil
 }
